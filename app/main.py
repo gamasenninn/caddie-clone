9397cd0af1de39e0375315f1b402896495410b3e
gamasenninn <gamasenninn@yahoo.co.jp>
2021-11-04 01:14:49 +0900

    upload 0.1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import sys
 
-from flask import Flask
+from flask import Flask,request,json, jsonify,Response,make_response
+from flask_cors import CORS, cross_origin
+import json
+import os
+import glob
 
 app = Flask(__name__)
-
+CORS(app, support_credentials=True)
 
 @app.route("/")
 def hello():
@@ -13,6 +17,43 @@ def hello():
     )
     return message
 
+@app.route("/upload",methods=['POST'])
+@cross_origin(supports_credentials=True)
+def upload_file():
+    if 'file' not in request.files:
+        make_response(jsonify({'result':'uploadFile is required.'}))
+    #message = request.json
+    f = request.files["file"]
+    id= request.form['fileId']
+    #print("file={}".format(f))
+    os.makedirs(f'./upload/{id}', exist_ok=True)
+    f.save(f'./upload/{id}/{f.filename}')
+    response = {
+        "text":"OK",
+        "fileId": id,
+        "filename": f.filename,      
+        "mimetype": f.mimetype,      
+    }
+    #return  jsonify(response)
+    json = request.get_json()
+    return  jsonify(response)
+
+    # return  jsonify(response)
+
+@app.route("/file_list/<fid>",methods=['GET'])
+@cross_origin(supports_credentials=True)
+def get_file_list(fid):
+    file_list = glob.glob(f'upload/{fid}/*')
+    app.logger.debug(file_list)
+    file_list = ["../"+f.replace('\\','/') for f in file_list ]
+    app.logger.debug(file_list)
+    j_flist = { "list": file_list}
+    message = jsonify(j_flist)
+    return message
+
+@app.route('/<f>')
+def proc(f):
+    return app.send_static_file('./'+f)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=80)
