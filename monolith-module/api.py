6fe39e2d469d387ea63eeb6ce780c3f03dfe9fd8
monomonosu <pojinyan@yahoo.co.jp>
2021-11-29 16:02:57 +0900

    全基本CRUD作成

--- monolith-module/api.py
+++ monolith-module/api.py
@@ -2,6 +2,7 @@ from app import app
 from models import *
 from flask import jsonify, request
 import json
+from datetime import date
 
 
 @app.route('/')
@@ -9,7 +10,7 @@ def index():
     return 'HelloWorld!'
 
 
-# -----得意先(Customer)-----
+# -----得意先(Customers)-----
 @app.route('/customer', methods=['GET'])
 def customer_index():
     customer = Customer.query.all()
@@ -76,19 +77,19 @@ def customer_destroy(id):
     return jsonify({"result": "OK", "id": id, "data": ''})
 
 
-# -----商品(item)-----
+# -----商品(Items)-----
 @app.route('/item', methods=['GET'])
 def item_index():
-    return jsonify(
-        [item.to_dict() for item in Item.query.all()]
-    )
+    items = Item.query.all()
+    return jsonify(ItemSchema(many=True).dump(items))
 
 
 @app.route('/item/<id>', methods=['GET'])
 def item_show(id):
     itemCount = Item.query.filter(Item.id == id).count()
     if itemCount:
-        return jsonify(Item.query.filter(Item.id == id).first().to_dict())
+        item = Item.query.filter(Item.id == id).first()
+        return jsonify(ItemSchema().dump(item))
     else:
         return jsonify([])
 
@@ -131,6 +132,353 @@ def item_destroy(id):
     item = Item.query.filter(Item.id == id).delete()
     db.session.commit()
     return jsonify({"result": "OK", "id": id, "data": ''})
+
+
+# -----請求書(Invoices)-----
+@app.route('/invoice', methods=['GET'])
+def invoice_index():
+    invoices = Invoice.query.all()
+    return jsonify(InvoiceSchema(many=True).dump(invoices))
+
+
+@app.route('/invoice/<id>', methods=['GET'])
+def invoice_show(id):
+    invoiceCount = Invoice.query.filter(Invoice.id == id).count()
+    if invoiceCount:
+        invoice = Invoice.query.filter(Invoice.id == id).first()
+        return jsonify(InvoiceSchema().dump(invoice))
+    else:
+        return jsonify([])
+
+
+@app.route('/invoice', methods=['POST'])
+def invoice_create():
+    data = request.json
+    newInvoice = Invoice(
+        customerId=data['customerId'],
+        applyNumber=data['applyNumber'],
+        applyDate=data['applyDate'],
+        expiry=data['expiry'],
+        title=data['title'],
+        memo=data['memo'],
+        remarks=data['remarks'],
+        isTaxExp=data['isTaxExp'],
+    )
+    db.session.add(newInvoice)
+    db.session.commit()
+    id = newInvoice.id
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/invoice/<id>', methods=['PUT'])
+def invoice_update(id):
+    data = request.json
+    invoice = Invoice.query.filter(Invoice.id == id).one()
+    invoice.customerId = data['customerId']
+    invoice.applyNumber = data['applyNumber']
+    invoice.applyDate = data['applyDate']
+    invoice.expiry = data['expiry']
+    invoice.title = data['title']
+    invoice.memo = data['memo']
+    invoice.remarks = data['remarks']
+    invoice.isTaxExp = data['isTaxExp']
+    db.commit()
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/invoice/<id>', methods=['DELETE'])
+def invoice_destroy(id):
+    invoice = Invoice.query.filter(Invoice.id == id).delete()
+    db.session.commit()
+    return jsonify({"result": "OK", "id": id, "data": ''})
+
+
+# 請求書＿商品(Invoice_Items)
+@app.route('/invoice_item', methods=['GET'])
+def invoice_item_index():
+    invoiceItems = Invoice_Item.query.all()
+    return jsonify(Invoice_ItemSchema(many=True).dump(invoiceItems))
+
+
+@app.route('/invoice_item/<id>', methods=['GET'])
+def invoice_item_show(id):
+    invoiceItemCount = Invoice_Item.query.filter(Invoice_Item.id == id).count()
+    if invoiceItemCount:
+        invoiceItem = Invoice_Item.query.filter(Invoice_Item.id == id).first()
+        return jsonify(Invoice_ItemSchema().dump(invoiceItem))
+    else:
+        return jsonify([])
+
+
+@app.route('/invoice_item', methods=['POST'])
+def invoice_item_create():
+    data = request.json
+    newInvoiceItem = Invoice_Item(
+        invoiceId=data['invoiceId'],
+        itemId=data['itemId'],
+        count=data['count'],
+    )
+    db.session.add(newInvoiceItem)
+    db.session.commit()
+    id = newInvoiceItem.id
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/invoice_item/<id>', methods=['PUT'])
+def invoice_item_update(id):
+    data = request.json
+    invoiceItem = Invoice_Item.query.filter(Invoice_Item.id == id).one()
+    invoiceItem.invoiceId = data['invoiceId']
+    invoiceItem.itemId = data['itemId']
+    invoiceItem.count = data['count']
+    db.commit()
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/invoice_item/<id>', methods=['DELETE'])
+def invoice_item_destroy(id):
+    invoiceItem = Invoice_Item.query.filter(Invoice_Item.id == id).delete()
+    db.session.commit()
+    return jsonify({"result": "OK", "id": id, "data": ''})
+
+
+# 見積書(Quotaions)
+@app.route('/quotaion', methods=['GET'])
+def quotaion_index():
+    quotaions = Quotaion.query.all()
+    return jsonify(QuotaionSchema(many=True).dump(quotaions))
+
+
+@app.route('/quotaion/<id>', methods=['GET'])
+def quotaion_show(id):
+    quotaionCount = Quotaion.query.filter(Quotaion.id == id).count()
+    if quotaionCount:
+        quotaion = Quotaion.query.filter(Quotaion.id == id).first()
+        return jsonify(QuotaionSchema().dump(quotaion))
+    else:
+        return jsonify([])
+
+
+@app.route('/quotaion', methods=['POST'])
+def quotaion_create():
+    data = request.json
+    newQuotaion = Quotaion(
+        customerId=data['customerId'],
+        applyNumber=data['applyNumber'],
+        applyDate=data['applyDate'],
+        expiry=data['expiry'],
+        title=data['title'],
+        memo=data['memo'],
+        remarks=data['remarks'],
+        isTaxExp=data['isTaxExp'],
+    )
+    db.session.add(newQuotaion)
+    db.session.commit()
+    id = newQuotaion.id
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/quotaion/<id>', methods=['PUT'])
+def quotaion_update(id):
+    data = request.json
+    quotaion = Quotaion.query.filter(Quotaion.id == id).one()
+    quotaion.customerId = data['customerId']
+    quotaion.applyNumber = data['applyNumber']
+    quotaion.applyDate = data['applyDate']
+    quotaion.expiry = data['expiry']
+    quotaion.title = data['title']
+    quotaion.memo = data['memo']
+    quotaion.remarks = data['remarks']
+    quotaion.isTaxExp = data['isTaxExp']
+    db.commit()
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/quotaion/<id>', methods=['DELETE'])
+def quotaion_destroy(id):
+    quotaion = Quotaion.query.filter(Quotaion.id == id).delete()
+    db.session.commit()
+    return jsonify({"result": "OK", "id": id, "data": ''})
+
+
+# 見積書＿商品(Quotaion_Items)
+@app.route('/quotaion_item', methods=['GET'])
+def quotaion_item_index():
+    quotaionItems = Quotaion_Item.query.all()
+    return jsonify(Quotaion_ItemSchema(many=True).dump(quotaionItems))
+
+
+@app.route('/quotaion_item/<id>', methods=['GET'])
+def quotaion_item_show(id):
+    quotaionItemCount = Quotaion_Item.query.filter(
+        Quotaion_Item.id == id).count()
+    if quotaionItemCount:
+        quotaionItem = Quotaion_Item.query.filter(
+            Quotaion_Item.id == id).first()
+        return jsonify(Quotaion_ItemSchema().dump(quotaionItem))
+    else:
+        return jsonify([])
+
+
+@app.route('/quotaion_item', methods=['POST'])
+def quotaion_item_create():
+    data = request.json
+    newQuotaionItem = Quotaion_Item(
+        quotaionId=data['quotaionId'],
+        itemId=data['itemId'],
+        count=data['count'],
+    )
+    db.session.add(newQuotaionItem)
+    db.session.commit()
+    id = newQuotaionItem.id
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/quotaion_item/<id>', methods=['PUT'])
+def quotaion_item_update(id):
+    data = request.json
+    quotaionItem = Quotaion_Item.query.filter(Quotaion_Item.id == id).one()
+    quotaionItem.quotaionId = data['quotaionId']
+    quotaionItem.itemId = data['itemId']
+    quotaionItem.count = data['count']
+    db.commit()
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/quotaion_item/<id>', methods=['DELETE'])
+def quotaion_item_destroy(id):
+    quotaionItem = Quotaion_Item.query.filter(Quotaion_Item.id == id).delete()
+    db.session.commit()
+    return jsonify({"result": "OK", "id": id, "data": ''})
+
+
+# メモ(Memos)
+@app.route('/memo', methods=['GET'])
+def memo_index():
+    memos = Memo.query.all()
+    return jsonify(MemoSchema(many=True).dump(memos))
+
+
+@app.route('/memo/<id>', methods=['GET'])
+def memo_show(id):
+    memoCount = Memo.query.filter(Memo.id == id).count()
+    if memoCount:
+        memo = Memo.query.filter(Memo.id == id).first()
+        return jsonify(MemoSchema().dump(memo))
+    else:
+        return jsonify([])
+
+
+@app.route('/memo', methods=['POST'])
+def memo_create():
+    data = request.json
+    newMemo = Memo(
+        title=data['title'],
+        content=data['content'],
+    )
+    db.session.add(newMemo)
+    db.session.commit()
+    id = newMemo.id
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/memo/<id>', methods=['PUT'])
+def memo_update(id):
+    data = request.json
+    memo = Memo.query.filter(Memo.id == id).one()
+
+    memo.title = data['title']
+    memo.content = data['content']
+
+    db.commit()
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/memo/<id>', methods=['DELETE'])
+def memo_destroy(id):
+    memo = Memo.query.filter(Memo.id == id).delete()
+    db.session.commit()
+    return jsonify({"result": "OK", "id": id, "data": ''})
+
+
+# 単位(Units)
+@app.route('/unit', methods=['GET'])
+def unit_index():
+    units = Unit.query.all()
+    return jsonify(UnitSchema(many=True).dump(units))
+
+
+@app.route('/unit/<id>', methods=['GET'])
+def unit_show(id):
+    unitCount = Unit.query.filter(Unit.id == id).count()
+    if unitCount:
+        unit = Unit.query.filter(Unit.id == id).first()
+        return jsonify(UnitSchema().dump(unit))
+    else:
+        return jsonify([])
+
+
+@app.route('/unit', methods=['POST'])
+def unit_create():
+    data = request.json
+    newUnit = Unit(
+        unitName=data['unitName'],
+    )
+    db.session.add(newUnit)
+    db.session.commit()
+    id = newUnit.id
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/unit/<id>', methods=['PUT'])
+def unit_update(id):
+    data = request.json
+    unit = Unit.query.filter(Unit.id == id).one()
+
+    unit.unitName = data['unitName']
+
+    db.commit()
+    return jsonify({"result": "OK", "id": id, "data": data})
+
+
+@app.route('/unit/<id>', methods=['DELETE'])
+def unit_destroy(id):
+    unit = Unit.query.filter(Unit.id == id).delete()
+    db.session.commit()
+    return jsonify({"result": "OK", "id": id, "data": ''})
+
+
+# 設定(Setting)
+@app.route('/setting', methods=['GET'])
+def setting_index():
+    setting = Setting.query.all()
+    return jsonify(SettingSchema(many=True).dump(setting))
+
+
+@app.route('/setting', methods=['PUT'])
+def setting_update():
+    data = request.json
+    setting = Setting.query.filter(Setting.id == 1).one()
+
+    setting.companyName = data['companyName']
+    setting.representative = data['representative']
+    setting.postNumber = data['postNumber']
+    setting.address = data['address']
+    setting.telNumber = data['telNumber']
+    setting.faxNumber = data['faxNumber']
+    setting.url = data['url']
+    setting.email = data['email']
+    setting.logoFilePath = data['logoFilePath']
+    setting.stampFilePath = data['stampFilePath']
+    setting.isDisplayQuotationLogo = data['isDisplayQuotationLogo']
+    setting.isDisplayInvoiceLogo = data['isDisplayInvoiceLogo']
+    setting.isDisplayDeliveryLogo = data['isDisplayDeliveryLogo']
+    setting.isDisplayQuotationStamp = data['isDisplayQuotationStamp']
+    setting.isDisplayInvoiceStamp = data['isDisplayInvoiceStamp']
+    setting.isDisplayDeliveryStamp = data['isDisplayDeliveryStamp']
+
+    db.commit()
+    return jsonify({"result": "OK", "id": id, "data": data})
 
 
 if __name__ == '__main__':
