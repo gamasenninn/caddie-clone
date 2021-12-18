424e1848a1bc3845074be114e39a4c923dc746ff
gamasenninn <gamasenninn@yahoo.co.jp>
2021-12-18 12:07:37 +0900

    pdf server

---
+++ pdfmaker/app.py
@@ -0,0 +1,42 @@
+# Server sample for Pdf Maker 
+# 
+#
+from flask import Flask,Response,make_response
+from pdf_maker import pdf_maker
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello Pdf serve"
+
+    
+@app.route('/pdfmaker')
+def makepdf():
+    with open( './data.json', mode='r', encoding='utf-8') as f:
+        d = json.load(f)
+
+    pdf_maker(d)
+    return f"This response is PDF Stream {d}"
+
+@app.route('/file/<file_name>')
+def file(file_name):
+    try:
+        with open( f"./pdf/{file_name}", mode='rb') as f:
+            pdfdata = f.read()
+            response = make_response(pdfdata)
+            response.headers['Content-Type'] = 'application/pdf'
+            response.headers['Content-Disposition'] = f'inline; filename={file_name}'
+            return response
+    except:
+        pass
+        return f"file error:{file_name}"
+
+    #return f"This response is PDF Stream {file_name}"
+
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5020, debug=True)
