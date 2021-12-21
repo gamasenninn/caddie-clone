5e6e59baa566444517b4717872bc758bd24fa8b7
gamasenninn <gamasenninn@yahoo.co.jp>
2021-12-21 15:58:59 +0900

    cors 対応

--- pdfmaker/app/main.py
+++ pdfmaker/app/main.py
@@ -5,8 +5,12 @@ from flask import Flask,make_response,redirect,request
 from pdf_maker import pdf_maker
 import json
 import uuid
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
+
 
 @app.route('/')
 def hello():
@@ -17,6 +21,7 @@ def test():
     return app.send_static_file('test-pdf.html')
 
 @app.route('/pdfmaker',methods=["GET","POST"])
+@cross_origin(supports_credentials=True)
 def makepdf():
 
     if request.method == "GET":
@@ -28,6 +33,7 @@ def makepdf():
         return alter_file_name
 
 @app.route('/pdfmaker/<json_file>')
+@cross_origin(supports_credentials=True)
 def makepdf_file(json_file):
     with open( f'./{json_file}.json', mode='r', encoding='utf-8') as f:
         d = json.load(f)
@@ -43,11 +49,13 @@ def makepdf_file(json_file):
     return alter_file_name
 
 @app.route('/pdf/<file>',methods=["GET"])
+@cross_origin(supports_credentials=True)
 def open_pdf(file):
     return app.send_static_file("pdf/"+file)
     
 #----ファイルを指定してpdfのレスポンスを返す場合（今回は使わない）---
 @app.route('/file/<file_name>')
+@cross_origin(supports_credentials=True)
 def file(file_name):
     try:
         with open( f"./pdf/{file_name}", mode='rb') as f:
