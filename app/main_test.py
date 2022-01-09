838f19a8d734c63b81e3fe030e05d5cedac64c98
monomonosu <pojinyan@yahoo.co.jp>
2022-01-09 19:51:14 +0900

    商品一覧表示まで動作確認

--- app/main_test.py
+++ app/main_test.py
@@ -61,6 +61,11 @@ def quotationPage():
     return app.send_static_file('quotation.html')
 
 
+@app.route('/item-page')
+def itemPage():
+    return app.send_static_file('item.html')
+
+
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0', port=5010)
