c1e1969c5f2ebc3bc690887f333ed67b73d3666f
gamasenninn <gamasenninn@yahoo.co.jp>
2022-01-21 19:24:43 +0900

    テスト組み込み

--- app/main_test.py
+++ app/main_test.py
@@ -3,8 +3,11 @@ import sys
 import json
 import uuid
 from flask import redirect, request
+from flask import Flask,request,json, jsonify,Response,make_response
+
 sys.path.append('../')
 import pdfmaker.app.pdf_maker as pd
+from upload.upload import make_thumb,chext,save_file,remove_files,get_flist
 
 
 @app.route('/test')
@@ -145,6 +148,36 @@ def invoiceDustPage():
 @app.route('/quotation-dust-page')
 def quotationDustPage():
     return app.send_static_file('quotation_dust.html')
+
+
+#--------- UPLOAD function ----------
+@app.route('/test-upload')
+def uptest():
+    return app.send_static_file('./uptest.html')
+
+@app.route("/upload",methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        make_response(jsonify({'result':'uploadFile is required.'}))
+    f = request.files["file"]
+    id= request.form['fileId'] 
+    dir_path = "./static/upload" 
+    return  jsonify(save_file(id,dir_path,f))
+
+@app.route("/delete-files",methods=['DELETE'])
+def delete_files():
+    dict_data = json.loads(request.data.decode())
+    return  jsonify(remove_files(dict_data))
+
+@app.route("/file-list/<fid>",methods=['GET'])
+def get_file_list(fid):
+
+    return jsonify(get_flist(fid,"./static/upload"))
+
+@app.route('/upload/<fid>/thumbs/<file>')
+def upimage(fid,file):
+    app.logger.debug(f"{fid}:{file}")
+    return app.send_static_file(f'./upload/{fid}/thumbs/{file}')
 
 
 if __name__ == '__main__':
