0c272697ca114b51c2ce6d8a5f94029ac030e26e
gamasenninn <gamasenninn@yahoo.co.jp>
2022-01-28 12:38:01 +0900

    DB接続・メニュー（ログイン/ログアウト）対応

--- app/static/component/sc-menu.js
+++ app/static/component/sc-menu.js
@@ -9,7 +9,7 @@ Vue.component('sc-menu',{
                 <b-button variant="danger" block @click="hide" style="float: right; width: 15%;">✖
                 </b-button>
                 <b-img src="https://free-materials.com/adm/wp-content/uploads/2020/10/logo_07-1024x1024.png"
-                    style="width: 100%; height: 50px; margin: 10px 0px;"></b-img>
+                    style="width: 50%; " center ></b-img>
                 <nav class="mt-3">
                     <b-nav vertical class="mb-4">
                         <b-button variant="info" block href="/home-page"><i class="fas fa-home"></i>　TOP
@@ -27,6 +27,10 @@ Vue.component('sc-menu',{
                                 class="far fa-copy"></i>　見積書</b-button>
                         <b-button variant="primary" block href="/memo-page"><i
                                 class="fas fa-book-open"></i>　メ　モ</b-button>
+                        <b-button variant="warning" block href="/login"><i
+                                class="fas fa-book-open"></i>　ログイン</b-button>
+                        <b-button variant="warning" block href="/logout"><i
+                                class="fas fa-book-open"></i>　ログアウト</b-button>
                     </b-nav>
                 </nav>
             </div>
