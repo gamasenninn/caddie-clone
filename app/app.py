c4f7b1d8703055772eb2c91be9c0bf54a124abe0
gamasenninn <gamasenninn@yahoo.co.jp>
2022-02-07 16:49:25 +0900

    ヘッダーをテンプレートにして外部化

--- app/app.py
+++ app/app.py
@@ -4,7 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 
-app = Flask(__name__)
+#app = Flask(__name__)
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='<%',
+        block_end_string='%>',
+        variable_start_string='%%',
+        variable_end_string='%%',
+        comment_start_string='<#',
+        comment_end_string='#>',
+    ))
+app = CustomFlask(__name__)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///flask_sample.db"
 
