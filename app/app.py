f37c527ae19d34eb9f3960dd5cd108f5b34cc428
monomonosu <pojinyan@yahoo.co.jp>
2022-03-25 13:09:27 +0900

    セッションタイムアウトを追加。

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 # 本体
+from datetime import timedelta
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 
 #app = Flask(__name__)
+
+
 class CustomFlask(Flask):
     jinja_options = Flask.jinja_options.copy()
     jinja_options.update(dict(
@@ -15,10 +18,13 @@ class CustomFlask(Flask):
         comment_start_string='<#',
         comment_end_string='#>',
     ))
+
+
 app = CustomFlask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///flask_sample.db"
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
