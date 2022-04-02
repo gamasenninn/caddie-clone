c010ad1ac31e9027da7e88d39ca6732daac73148
monomonosu <pojinyan@yahoo.co.jp>
2022-04-02 17:21:25 +0900

    指摘箇所修正。ヘッダーのコンポーネント化。内部で持てるメソッドは内部で持つように変更。

---
+++ app/static/component/menu-header.js
@@ -0,0 +1,33 @@
+Vue.component('menu-header', {
+    template: `
+    <div>
+        <b-card class="fixed-top header" style="background: rgba(255,255,255,0.5);">
+            <b-row>
+                <b-col></b-col>
+                <b-col cols="11">
+                    <b-row>
+                        <b-col>
+                            <router-link to="?page=store">
+                                <b-button pill variant="success" class="mr-3" @click="InvoiceAddRow();">＋新規作成
+                                </b-button>
+                            </router-link>
+                        </b-col>
+                        <b-col class="text-right">
+                            <sc-menu v-if="modeName!='mw'"></sc-menu>
+                        </b-col>
+                    </b-row>
+                </b-col>
+                <b-col></b-col>
+            </b-row>
+        </b-card>
+    </div>
+    `,
+    props: {
+        InvoiceAddRow: Function,
+    },
+    methods: {
+        modeName: function () {
+            return localStorage.getItem('wMode');
+        },
+    },
+})
