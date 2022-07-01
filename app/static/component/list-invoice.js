d00140d7b1617e5487b4ee3bd57b50054dd78cf7
monomonosu <pojinyan@yahoo.co.jp>
2022-07-01 16:03:11 +0900

    一覧テーブルをボーダレス＋ストライプに。主要ページのみ適用。

--- app/static/component/list-invoice.js
+++ app/static/component/list-invoice.js
@@ -111,7 +111,7 @@ var indicateCount = Vue.component('indicate-count', {
 Vue.component('invoice-list', {
     template: `
     <div>
-        <b-table responsive hover small id="invoicetable" sort-by="ID" small label="Table Options"
+        <b-table borderless responsive hover small id="invoicetable" sort-by="ID" small label="Table Options"
             :items=invoicesIndicateIndex :sort-by.sync="this.sortByInvoices" :sort-desc.sync="this.sortDesc" :fields="[
         {  key: 'update', label: '' },
         {  key: 'id', thClass: 'd-none', tdClass: 'd-none' },
@@ -173,7 +173,7 @@ Vue.component('invoice-list', {
 Vue.component('invoice-list-payment', {
     template: `
     <div>
-        <b-table responsive hover small id="invoicetable" label="Table Options"
+        <b-table borderless responsive hover small id="invoicetable" label="Table Options"
             :items=invoicesIndicateIndex :sort-by.sync="this.sortByInvoices" :sort-desc.sync="this.sortDesc" :fields="[
         {  key: 'update', label: '' },
         {  key: 'id', thClass: 'd-none', tdClass: 'd-none' },
