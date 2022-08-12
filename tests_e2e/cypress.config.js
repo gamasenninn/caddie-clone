af132b1e5fefd9d5e7502b0838e1cc42ed2a7211
gamasenninn <61409504+gamasenninn@users.noreply.github.com>
2022-08-12 18:27:40 +0900

    Update cypress.config.js

--- tests_e2e/cypress.config.js
+++ tests_e2e/cypress.config.js
@@ -3,5 +3,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    supportFile: false,
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
   },
 };
